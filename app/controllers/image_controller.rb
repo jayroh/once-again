80472dd067e0e0c2c8233d25e7d80898aa3d7a13
Joel Oliveira <joel.oliveira@gmail.com>
2024-03-11 21:05:43 -0400

    Handle errors with a redirect

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,19 +1,23 @@
 # frozen_string_literal: true
 
 class ImageController < ApplicationController
+  rescue_from JpgCreator::Errors::InvalidSlug, with: :invalid_slug
+
   def show
     jpg_creator = JpgCreator.new(image_slug)
     jpg_creator.validate_slug!
     jpg_creator.create
 
-    send_file jpg_creator.path,
-      type: 'image/jpg',
-      disposition: 'inline'
+    send_file jpg_creator.path, type: 'image/jpg', disposition: 'inline'
   end
 
   private
 
   def image_slug
     params[:id].dasherize.downcase
+  end
+
+  def invalid_slug
+    redirect_to '/a-valid-path-with-letters-dashes-and-spaces.jpg'
   end
 end
